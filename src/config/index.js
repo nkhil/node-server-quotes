1a20c1dd1944dd593c24b917a08ca8db32303e6a
Nikhil Vijayan <nkhilv@gmail.com>
2019-06-14 21:09:46 +0100

    fix: delete config

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  express: {
-    port: parseInt(process.env.EXPRESS_PORT, 10) || 3000
-  },
-  // defines the application environment
-  environment: process.env.ENVIRONMENT,
-  project: process.env.NAME || 'node_db'
-};
