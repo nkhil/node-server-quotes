d7d9e02ff04eecf6e67d4131093d19b9f16b05f4
Nikhil Vijayan <nkhilv@gmail.com>
2019-06-15 15:09:54 +0100

    chore: log the port the app is running on

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const app = require('./src/app');
 
 // Making environment variables available to the rest of the application
 require('dotenv').config({ path: '.env' });
+
+app.listen(config.express.port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Express running on port ${config.express.port}`);
+});
