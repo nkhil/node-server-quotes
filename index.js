b5e2b6fc45970b21fd422ca33f47615f51bf4d37
Nikhil Vijayan <nkhilv@gmail.com>
2019-06-15 15:10:06 +0100

    feat: add db connection

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const mongoose = require('mongoose');
 const config = require('./src/config');
 const app = require('./src/app');
 
 // Making environment variables available to the rest of the application
 require('dotenv').config({ path: '.env' });
+
+// connect to your db
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 Promises
+mongoose.connection.on('error', err => {
+  // eslint-disable-next-line no-console
+  console.log(`ERROR OCCURED: ${err.message}`);
+});
 
 app.listen(config.express.port, () => {
   // eslint-disable-next-line no-console
